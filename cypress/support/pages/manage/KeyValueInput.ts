2448bc2ba508d27c061a8369d4bc13edde277c96
rcunday <rwcunday@gmail.com>
2023-08-07 09:43:44 -0600

    add realm role tests

---
+++ cypress/support/pages/manage/KeyValueInput.ts
@@ -0,0 +1,47 @@
+//import type { KeyValueType } from "../../../../../src/components/key-value-form/key-value-convert";
+
+type KeyValueType = { key: string; value: string };
+
+export default class KeyValueInput {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  fillKeyValue({ key, value }: KeyValueType) {
+    cy.findByTestId(`${this.name}-add-row`).click();
+
+    cy.findAllByTestId(`${this.name}-key`)
+      .its("length")
+      .then((length) => {
+        this.keyInputAt(length - 1).type(key);
+        this.valueInputAt(length - 1).type(value);
+      });
+
+    return this;
+  }
+
+  deleteRow(index: number) {
+    cy.findAllByTestId(`${this.name}-remove`).eq(index).click();
+    return this;
+  }
+
+  validateRows(numberOfRows: number) {
+    cy.findAllByTestId(`${this.name}-key`).should("have.length", numberOfRows);
+    return this;
+  }
+
+  save() {
+    cy.findByTestId("save-attributes").click();
+    return this;
+  }
+
+  keyInputAt(index: number) {
+    return cy.findAllByTestId(`${this.name}-key`).eq(index);
+  }
+
+  valueInputAt(index: number) {
+    return cy.findAllByTestId(`${this.name}-value`).eq(index);
+  }
+}
