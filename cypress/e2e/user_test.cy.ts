e15ed16c7bc80ebd56473c1ecb43ccf3b598e43a
rcunday <rwcunday@gmail.com>
2023-08-07 08:52:36 -0600

    add addtional user tests

--- cypress/e2e/user_test.cy.ts
+++ cypress/e2e/user_test.cy.ts
@@ -11,9 +11,11 @@ import CredentialsPage from '../support/pages/manage/users/CredentialsPage'
 import ListingPage from '../support/pages/ListingPage'
 import AttributesTab from '../support/pages/manage/AttributesTab'
 import UserGroupsPage from '../support/pages/manage/users/UserGroupsPage'
+import ModalUtils from '../support/util/ModalUtils'
 
 let groupsList: string[] = []
 let itemId = 'user_crud'
+const itemCredential = 'Password';
 
 describe('User Creation', () => {
   const loginPage = new LoginPage()
@@ -25,6 +27,7 @@ describe('User Creation', () => {
   const listingPage = new ListingPage()
   const attributesTab = new AttributesTab()
   const userGroupsPage = new UserGroupsPage()
+  const modalUtils = new ModalUtils()
 
   let itemIdWithGroups = 'user_with_groups_crud'
   let itemIdWithCred = 'user_crud_cred'
@@ -212,5 +215,102 @@ describe('User Creation', () => {
     cy.findByTestId('user-consents-tab').click()
     cy.findByTestId('empty-state').contains('No consents')
   })
-})
 
+
+  it("Reset credential of User with empty state", () => {
+    listingPage.goToItemDetails(itemId)
+    credentialsPage
+      .goToCredentialsTab()
+      .clickEmptyStateResetBtn()
+      .fillResetCredentialForm()
+    masthead.checkNotificationMessage(
+      "Failed: Failed to send execute actions email",
+    )
+  })
+
+
+  it("Reset credential of User with existing credentials", () => {
+    listingPage.goToItemDetails(itemIdWithCred)
+    credentialsPage
+      .goToCredentialsTab()
+      .clickResetBtn()
+      .fillResetCredentialForm()
+
+    masthead.checkNotificationMessage(
+      "Failed: Failed to send execute actions email",
+    )
+  })
+
+  it("Edit credential label", () => {
+    listingPage.goToItemDetails(itemIdWithCred)
+    credentialsPage
+      .goToCredentialsTab()
+      .clickEditCredentialLabelBtn()
+      .fillEditCredentialForm()
+      .clickEditConfirmationBtn();
+
+    masthead.checkNotificationMessage(
+      "The user label has been changed successfully.",
+    )
+  })
+
+  it("Show credential data dialog", () => {
+    listingPage.goToItemDetails(itemIdWithCred)
+    credentialsPage
+      .goToCredentialsTab()
+      .clickShowDataDialogBtn()
+      .clickCloseDataDialogBtn()
+  })
+
+  it("Delete credential", () => {
+    listingPage.goToItemDetails(itemIdWithCred);
+    credentialsPage.goToCredentialsTab();
+
+    cy.wait(2000);
+    listingPage.deleteItem(itemCredential);
+    modalUtils.checkModalTitle("Delete credentials?").confirmModal();
+
+    masthead.checkNotificationMessage(
+      "The credentials has been deleted successfully.",
+    )
+  })
+
+  it("Delete user from search bar test", () => {
+    // Delete
+    sidebarPage.waitForPageLoad()
+
+    listingPage.searchItem(itemId).itemExist(itemId)
+    listingPage.deleteItemFromSearchBar(itemId)
+
+    modalUtils.checkModalTitle("Delete user?").confirmModal()
+
+    masthead.checkNotificationMessage("The user has been deleted")
+    sidebarPage.waitForPageLoad()
+
+    listingPage.itemExist(itemId, false)
+  })
+
+  it("Delete user with groups test", () => {
+    // Delete
+    listingPage.deleteItem(itemIdWithGroups)
+
+    modalUtils.checkModalTitle("Delete user?").confirmModal()
+
+    masthead.checkNotificationMessage("The user has been deleted")
+    sidebarPage.waitForPageLoad()
+
+    listingPage.itemExist(itemIdWithGroups, false)
+  })
+
+  it("Delete user with credential test", () => {
+    // Delete
+    listingPage.deleteItem(itemIdWithCred)
+
+    modalUtils.checkModalTitle("Delete user?").confirmModal()
+
+    masthead.checkNotificationMessage("The user has been deleted")
+    sidebarPage.waitForPageLoad()
+
+    listingPage.itemExist(itemIdWithCred, false)
+  })
+})
